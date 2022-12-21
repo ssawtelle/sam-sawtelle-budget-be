4b4df516a2dec56439f53cf0e246bdfb7d58a0fd
ssawtelle <ssawtelle10@gmail.com>
2022-12-21 10:07:58 -0600

    Initial Commit

---
+++ src/main/java/net/yorksolutions/samsawtellebudgetfe2/SamSawtelleBudgetFe2Application.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.samsawtellebudgetfe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamSawtelleBudgetFe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamSawtelleBudgetFe2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/yorksolutions/samsawtellebudgetfe2/SamSawtelleBudgetFe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.samsawtellebudgetfe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamSawtelleBudgetFe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
